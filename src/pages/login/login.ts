4599eb6e0cccb489692e1bc95556d2d2567df3e2
Eliel <eliel7114@gmail.com>
2017-06-25 19:15:02 +0300

    Added validations and an option to resend the sms code.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import firebase from 'firebase';
 import { AuthService } from '../../providers/auth-service/auth-service';
 import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+const RESEND_TIMER_TICK = 1000; // in ms
+const RESEND_TIMER_PERIOD = 60; // in seconds
 
 @Component({
   selector: 'page-login',
@@ -56,26 +60,130 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
     ])
   ]
 })
-export class LoginPage {
+export class LoginPage implements OnInit {
   logoState: string = "in";
   cloudState: string = "in";
   loginState: string = "in";
   formState: string = "in";
   smsSent: boolean = false;
+  resendTimer: number = 0;
   private recaptchaVerifier:firebase.auth.RecaptchaVerifier;
+
+  signupForm: FormGroup;
+  phoneNumberControl: FormControl;
+
+  loginForm: FormGroup;
+  confirmCodeControl: FormControl;
+  displayNameControl: FormControl;
+  cidControl: FormControl;
 
   constructor(public navCtrl: NavController, private authService: AuthService) {
   }
 
   ionViewDidLoad() {
-    this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    this.initRecaptcha();
   }
 
-  async signUp(phoneNumber: number){
-    this.smsSent = await this.authService.signUp(phoneNumber, this.recaptchaVerifier);
+  private initRecaptcha(): boolean {
+    if (!this.recaptchaVerifier) {
+      this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+      return true;
+    }
+    return false;
+  }
+
+  private clearRecaptcha() {
+    this.recaptchaVerifier.clear();
+    this.recaptchaVerifier = null;
+  }
+
+  async signUp(phoneNumber: number, resend:boolean){
+    if (resend && this.resendTimer > 0) {
+      return;
+    }
+
+    this.initRecaptcha();
+    let smsSent: boolean = await this.authService.signUp(phoneNumber, this.recaptchaVerifier);
+    this.resendTimer = RESEND_TIMER_PERIOD;
+    if (!resend) {
+      this.smsSent = smsSent;
+    }
+    this.setResendTimerTimeout();
+    this.clearRecaptcha();
+  }
+
+  resendTimerTimeout() {
+    this.resendTimer--;
+    if (this.resendTimer > 0) {
+      this.setResendTimerTimeout();
+    }
+  }
+  private setResendTimerTimeout() {
+    setTimeout(this.resendTimerTimeout.bind(this), RESEND_TIMER_TICK);
+  }
+
+  formatTimer(timer:number): string {
+    return this.formatNumber(Math.floor(timer / 60)) + ":" + this.formatNumber(timer % 60);
+  }
+  formatNumber(n:number):string {
+    let s: string = "";
+    if (n < 10) {
+      s += "0";
+    }
+    s += n;
+    return s;
   }
 
   confirmLogin(confirmCode:string, displayName:string, cid:string){
     this.authService.confirm(confirmCode, displayName, cid);
+  }
+
+  shouldShowControlErrors(control:FormControl){
+    return control.invalid && (control.touched || control.dirty);
+  }
+
+  ngOnInit() {
+    this.createFormsControls();
+    this.createForms();
+  }
+
+  createFormsControls() {
+    this.phoneNumberControl = new FormControl(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.pattern('5\\d{8}')
+      ])
+    );
+
+    this.confirmCodeControl = new FormControl(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.pattern('\\d{6}')
+      ])
+    );
+    this.displayNameControl = new FormControl(
+      '',
+      Validators.required
+    );
+    this.cidControl = new FormControl(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.pattern('\\d{9}')
+      ])
+    );
+  }
+
+  createForms() {
+    this.signupForm = new FormGroup({
+      phoneNumberControl: this.phoneNumberControl
+    });
+    this.loginForm = new FormGroup({
+      confirmCodeControl: this.confirmCodeControl,
+      displayNameControl: this.displayNameControl,
+      cidControl: this.cidControl
+    });
   }
 }
